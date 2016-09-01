848a9ca11a2f3e4446b00689073c739e3f887d79
DrCesar <josuejacobstercero@gmail.com>
2016-08-31 19:50:21 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -39,7 +39,28 @@ public class Main {
             System.out.println("Ingrese el nombre del usuario.");
             String name = scan.nextLine();
             
-            System.out.println("Ingrese 1 si es desarollador Java, 0 si no.");
+            System.out.println("Ingrese 1 si es desarrollador de ese lenguaje 0 si no. Orden Java Web Cel.\n Ejemplo 101 no dice que es desarrollador de Java y de Cel pero no Web.");
+            String info = scan.nextLine();
+            info.replaceAll("\\s+","");
+            for(int i = 0; i < info.length(); i++){
+                if(info.charAt(i) == '1')
+                    switch(i){
+                        case 0:
+                            jcfJava.add(name);
+                            contJava++;
+                            break;
+                        case 1:
+                            jcfWeb.add(name);
+                            contWeb++;
+                            break;
+                        case 2:
+                            jcfCel.add(name);
+                            contCel++;
+                            break;    
+                    }
+            }
+                
+            /*System.out.println("Ingrese 1 si es desarollador Java, 0 si no.");
             String isJava = scan.nextLine();
             if(isJava.equals("1")){
                 jcfJava.add(name);
@@ -58,20 +79,24 @@ public class Main {
             if(isCel.equals("1")){
                 jcfCel.add(name);
                 contCel++;
-            }
+            }*/
             System.out.println("Ingrese 1 si quiere seguir ingresando usuarios, 0 si no.");
             String haySig = scan.nextLine();
             if(!haySig.equals("1"))
                 seguirPidiendo = false;
         }
         
-        System.out.println("Los programadores de Java, Web, Cel son: ");
+        
         Iterator itrJava = jcfJava.iterator();
         Iterator itrWeb = jcfWeb.iterator();
         Iterator itrCel = jcfCel.iterator();
         Object obJava;
         Object obWeb;
         Object obCel;
+        
+        
+        
+        System.out.println("Los programadores de Java, Web, Cel son: ");
         while(itrJava.hasNext()) {
             obJava = itrJava.next();
             
@@ -79,10 +104,10 @@ public class Main {
                 System.out.println(obJava);
         }
         
+        
+        
         System.out.println("Los programadores de Java y no de Web son: ");
         itrJava = jcfJava.iterator();
-        itrWeb = jcfWeb.iterator();
-        itrCel = jcfCel.iterator();
         while(itrJava.hasNext()) {
             obJava = itrJava.next();
             
@@ -90,10 +115,10 @@ public class Main {
                 System.out.println(obJava);
         }
         
+        
+        
         System.out.println("Los programadores de Web y Cel y no de Java son: ");
-        itrJava = jcfJava.iterator();
         itrWeb = jcfWeb.iterator();
-        itrCel = jcfCel.iterator();
         while(itrWeb.hasNext()) {
             obWeb = itrWeb.next();
             
@@ -101,16 +126,18 @@ public class Main {
                 System.out.println(obWeb);
         }
         
+        
+        
         System.out.println("Los programadores de Web o Cel y no de Java son: ");
-        itrJava = jcfJava.iterator();
         itrWeb = jcfWeb.iterator();
-        itrCel = jcfCel.iterator();
         while(itrWeb.hasNext()) {
             obWeb = itrWeb.next();
             
             if(!jcfJava.contains(obWeb) && (jcfWeb.contains(obWeb) || jcfCel.contains(obWeb)))
                 System.out.println(obWeb);
         }
+        
+        
         
         itrJava = jcfJava.iterator();
         boolean isSubCon = true;
@@ -124,5 +151,37 @@ public class Main {
             System.out.println("Los programadores de Java son un subconjunto de los de Web.");
         else
             System.out.println("Los programadores de Java no son un subconjunto de los de Web.");
+    
+        
+        
+        if(contJava >= contWeb && contJava >= contCel){
+            System.out.println("El conjunto con mayor cantidad de desarrolladores es Java.");
+            itrJava = jcfJava.iterator();
+            while(itrJava.hasNext()) {
+                obJava = itrJava.next();
+                System.out.println(obJava);
+            } 
+        }else{
+            if(contWeb >= contCel){
+                System.out.println("El conjunto con mayor cantidad de desarrolladores es Web.");
+                itrWeb = jcfWeb.iterator();
+                while(itrWeb.hasNext()) {
+                    obWeb = itrWeb.next();
+                    System.out.println(obWeb);
+                }
+            }else{
+                System.out.println("El conjunto con mayor cantidad de desarrolladores es Cel.");
+                itrCel = jcfCel.iterator();
+                while(itrCel.hasNext()) {
+                    obCel = itrCel.next();
+                    System.out.println(obCel);
+                }
+            }  
+        }
+        
+        
+        
+        
     }
+    
 }
